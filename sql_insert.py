8805832fb81e90fe39be67408af8ff334f831b42
0bscurity <wifi.x.smasher1000@gmail.com>
2021-09-22 13:50:19 -0400

    Initial commit

---
+++ sql_insert.py
@@ -0,0 +1,29 @@
+import sqlite3
+import datetime
+
+conn = sqlite3.connect('transactions.sqlite')
+cur = conn.cursor()
+
+
+def input_data():
+    amt = input("Transaction amount: ")
+    category = input("Transaction type: ")
+    comment = input("Transaction comment: ")
+    date = datetime.datetime.now()
+    cur.execute("INSERT INTO transactions (trans_amt, trans_date, trans_type, trans_comment) "
+                "VALUES (?,?,?,?)", (float(amt), date, category, comment))
+
+    conn.commit()
+
+
+def read_db():
+    cur.execute('SELECT * FROM transactions')
+    data = cur.fetchall()
+    for row in data:
+        print(row)
+
+
+input_data()
+cur.close()
+read_db()
+
