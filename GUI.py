e2f490fd037394ff3bb430ad20614adf96c19fb6
0bscurity <vestorlockwood@gmail.com>
2021-10-06 12:18:03 -0400

    Used objet-oriented approach for GUI code... switched to class

--- GUI.py
+++ GUI.py
@@ -1,6 +1,11 @@
 from datetime import datetime
 from tkinter import *
 from tkcalendar import Calendar
+import sqlite3
+from datetime import datetime
+
+conn = sqlite3.connect('transactions.sqlite')
+cur = conn.cursor()
 
 
 class BudgetGUI:
@@ -12,31 +17,42 @@ class BudgetGUI:
         cal = Calendar(root, selectmode="day")
         cal.pack(pady=20)
 
+        date = cal.get_date()
+
         self.date1_button = Button(root, text="Get First Date",
-                                   command=lambda: date1.config(text="First Date is: " + cal.get_date())).pack(pady=20)
+                                   command=lambda: d1_label.config(text="First Date is: " +
+                                                                        date)).pack(pady=20)
 
         self.date2_button = Button(root, text="Get Second Date",
-                                   command=lambda: date2.config(text="Second Date is: " + cal.get_date())).pack(pady=20)
+                                   command=lambda: d2_label.config(text="Second Date is: " +
+                                                                        date)).pack(pady=20)
 
-        Button(root, text="Find Transactions",
-               command=lambda: trans_range.config(text="Transactions in date range: " + cal.get_date())).pack(pady=20)
+        self.trans_button = Button(root, text="Find Transactions",
+                                   command=lambda: trans_range.config(text="Transactions in date range: " +
+                                                                           get_dates())).pack(pady=20)
 
-        date1 = Label(root, text="")
-        date1.pack(pady=20)
+        d1_label = Label(root, text="")
+        d1_label.pack(pady=20)
 
-        date2 = Label(root, text="")
-        date2.pack(pady=20)
+        d2_label = Label(root, text="")
+        d2_label.pack(pady=20)
 
         trans_range = Label(root, text="")
         trans_range.pack(pady=20)
+
+        def get_dates():
+            cur.execute('SELECT * from transactions where trans_date BETWEEN "2021-10-01 14:04:27.182177" '
+                        'AND "2021-10-01 14:11:19.581165"')
+            range = cur.fetchall()
+            str_range = str(range)
+            return str_range
 
         menu = Menu(root)
         root.config(menu=menu)
 
         file_menu = Menu(menu)
-        file_menu.add_command(label="Item")
         file_menu.add_command(label="Exit", command=exit)
-        menu.add_cascade(label="File", menu=file_menu)
+        menu.add_cascade(label="Actions", menu=file_menu)
 
     def none(self):
         pass
